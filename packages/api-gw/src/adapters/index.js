15fe83b788bdc4961715cb48d53e2bb8406f4d05
oscar <oscar.pay4@gmail.com>
2021-07-31 12:33:53 -0500

    Changes to support new endpoint with statistics-module

--- packages/api-gw/src/adapters/index.js
+++ packages/api-gw/src/adapters/index.js
@@ -1,7 +1,9 @@
 const healthyChecks = require('./healthyChecks/healthyChecks');
 const link = require('./link/link');
+const statisticLink = require('./statisticLink/statisticLink');
 
 module.exports = {
   healthyChecks,
-  link
+  link,
+  statisticLink
 };
