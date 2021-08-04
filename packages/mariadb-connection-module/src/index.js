3eaa672d908ce196ba370dd3e9bc46d0f8310ce6
jinfanv <infanpearl@gmail.com>
2021-08-04 00:19:02 -0500

    Testing connection to database

--- packages/mariadb-connection-module/src/index.js
+++ packages/mariadb-connection-module/src/index.js
@@ -1,4 +1,15 @@
 const knex = require('knex');
 const databaseConfig = require('../src/config/knexfile');
 const DB = knex(databaseConfig);
+const logger = require('./utils/logger');
+
+DB.raw('select 1+1 as result')
+    .then(() => {
+        logger.info(`[pl-mongodbconnection-module]: Mongoose connection is opened to ${databaseConfig.connection.host}`)
+    })
+    .catch(err => {
+        logger.error(`[pl-mariadbconnection-module]: Connection error : ${err.message}`);
+        process.exit(1)
+    })
+
 module.exports = DB;
