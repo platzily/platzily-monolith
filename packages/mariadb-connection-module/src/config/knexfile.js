9013cffe147f6b5c12efd05f3f67ed20869d0c7a
jinfanv <infanpearl@gmail.com>
2021-09-06 22:22:05 -0500

    Setting default to knex

--- packages/mariadb-connection-module/src/config/knexfile.js
+++ packages/mariadb-connection-module/src/config/knexfile.js
@@ -5,16 +5,13 @@ const config = {
     DB_HOST: process.env[`${environment}DB_HOST`],
     DB_USER: process.env[`${environment}DB_USER`],
     DB_PASSWORD: process.env[`${environment}DB_PASSWORD`],
-    DB_NAME: process.env[`${environment}DB_NAME`],
-    CONNECTION_POOL_SIZE: parseInt(process.env[`${environment}CONNECTION_POOL_SIZE`]) || 10
+    DB_NAME: process.env[`${environment}DB_NAME`]
 }
 
 assert(config.DB_HOST, 'DB_HOST must be provided');
 assert(config.DB_USER, 'DB_USER must be provided');
 assert(config.DB_PASSWORD, 'DB_PASSWORD must be provided');
 assert(config.DB_NAME, 'DB_NAME must be provided');
-
-const timeout = 30000;
 
 module.exports = {
     client: 'mysql2',
@@ -23,14 +20,6 @@ module.exports = {
         user: config.DB_USER,
         password: config.DB_PASSWORD,
         database: config.DB_NAME
-    },
-    pool: {
-        min: 1,
-        max: config.CONNECTION_POOL_SIZE,
-        idleTimeoutMillis: timeout,
-        createTimeoutMillis: timeout,
-        acquireTimeoutMillis: timeout,
-    },
-    acquireConnectionTimeout: timeout,
-    connectionTimeout: timeout
+    }
 }
+
