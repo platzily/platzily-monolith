fb6f2ce76211b528491a41d9f98b9732bf083dd6
Jairo Salazar <jairosalazar1280@gmail.com>
2021-11-10 23:49:39 -0500

    feat(get-user) : Initial User DTO Interface

---
+++ packages/users-module/src/dto/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    firstname: string;
+    lastname: string;
+    description: string;
+    email: string;
+    rol: string;
+    image: string;
+    is_active: boolean;
+    reason: string;
+}
