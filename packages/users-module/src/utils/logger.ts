18411b1d83b20cd2e07d5cff7b2483059647cdd3
Jairo Salazar <jairosalazar1280@gmail.com>
2021-11-15 12:18:40 -0500

    feat(get-user): Add logger

---
+++ packages/users-module/src/utils/logger.ts
@@ -0,0 +1,19 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+export {
+    logger
+} 
+
