ee37f8475ec3a825fc4d061ebf8a1a38a7ed7a31
Jairo Salazar <jairosalazar1280@gmail.com>
2021-11-15 12:23:03 -0500

    feat(get-user): Implementing getUser use case with bussiness logic associated and handling exception

--- packages/users-module/src/usesCases/user.ts
+++ packages/users-module/src/usesCases/user.ts
@@ -1,9 +1,23 @@
-import { User } from "../entities/user";
+import { User, UserEntity } from "../dto/user";
+import { BusinessError } from "../utils/BusinessError";
+import { REGEX_EMAIL_PATTERN,errorTypes } from "../utils/constants";
+import { logger } from "../utils/logger";
 
-function getUser(user:User,email:string){
+async function getUser(userModel:UserEntity,email:string): Promise<User>{
 
+    if(!REGEX_EMAIL_PATTERN.test(email)){
+        throw new BusinessError(errorTypes.EMAIL_NOT_VALID, 'users-module');
+    }
+
+    try {
+        let user:User = await userModel.get(email);
+        return user;
+    } catch (error) {
+        logger.error('[pl-user-module]: Error getting user from database: ', error.message);
+        throw new BusinessError(errorTypes.READ_DATABASE_ERROR, 'users-module');
+    }
 }
 
-export class UserUsesCases {
+export {
     getUser
 }
