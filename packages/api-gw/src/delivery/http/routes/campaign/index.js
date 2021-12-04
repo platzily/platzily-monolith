da66c872dd9f44bc4a06ac80899def3bfb484e24
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2021-12-04 10:58:02 -0600

    ✨ [Introduce] route readAllCampaigns

--- packages/api-gw/src/delivery/http/routes/campaign/index.js
+++ packages/api-gw/src/delivery/http/routes/campaign/index.js
@@ -3,10 +3,10 @@ const schema = require('./schema')
 
 async function CampaignRouter(fastify) {
     fastify.post('/campaign/', schema.postSchema , campaign.createCampaign);
-    fastify.get('/campaign/:id', schema.getSchema, campaign.detailCampaign);
+    fastify.get('/campaign/', schema.getSchema, campaign.readAllCampaigns);
+    fastify.get('/campaign/:id', schema.getSchema, campaign.readCampaignById);
     fastify.put('/campaign/:id', schema.putSchema, campaign.updateCampaign);
     fastify.delete('/campaign/:id', schema.getSchema, campaign.deleteCampaign);
 }
-
 
 module.exports = CampaignRouter;
